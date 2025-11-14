8db4f6a30e9e9b700ad21734a7f4103973f79f9a
CelioXavier737 <cxavier737@gmail.com>
2025-11-14 11:52:51 +0000

    Commit incial com o programa funcional do nivel novato

--- war.c
+++ war.c
@@ -29,6 +29,29 @@
 // Funções de lógica principal do jogo:
 // Função utilitária:
 
+// COMENTARIO DO ALUNO: Aqui eu começo meu código
+// COMENTARIO DO ALUNO: Primeiro eu import a bibliotecas
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // uso essa para manipular os fgets()
+
+
+// COMENTARIO DO ALUNO: crio um conjunto de constantes para facilitar a escalabilidade e manuntenção
+
+#define TAM_NOME 30 // tamanho do campo nome dos territorios
+#define TAM_COR 10 // tamanho do campo cor dos territorios
+#define QTD_TERRITORIOS 5 // numero de territorios
+
+// COMENTARIO DO ALUNO: agora crio a Struct
+
+typedef struct {
+    char nome[TAM_NOME];
+    char cor[TAM_COR];
+    int tropas;
+} Territorio;
+
+
 // --- Função Principal (main) ---
 // Função principal que orquestra o fluxo do jogo, chamando as outras funções em ordem.
 int main() {
@@ -50,6 +73,59 @@ int main() {
 
     // 3. Limpeza:
     // - Ao final do jogo, libera a memória alocada para o mapa para evitar vazamentos de memória.
+
+
+// COMENTARIO DO ALUNO: Aqui começo a codificar o main
+// COMENTARIO DO ALUNO:criando o vetor dos territorios e declarando as variáveis
+
+    Territorio territorios[QTD_TERRITORIOS]; // usei o mesmo nome mas com capitular e no plural para diferenciar e ficar mais identificavel qdo se trata do nome da estrutura e qdo é o vetor
+    int i; // variavel usada para controle dos loops
+
+// COMENTARIO DO ALUNO: Banner de informação para o usuário
+
+    printf("=============================================\n");
+    printf("       SISTEMA DE CADASTRO DE TERRITORIOS\n");
+    printf("=============================================\n\n");
+
+//  COMENTARIO DO ALUNO: Entrada de dados: leitura dos 5 territórios
+
+    for (i = 0; i < QTD_TERRITORIOS; i++){
+        printf("Dados do Territorio: %d\n",i+1);
+
+        printf("Nome do Territorio:");
+        fgets(territorios[i].nome,TAM_NOME,stdin);                    // solitica a informação do nome do territorio    
+        territorios[i].nome[strcspn(territorios[i].nome,"\n")] = '\0';  // a diferenca de uso da aspa dupla e simples deve-se ao fato de um lugar se referir a cadeia de strings e outro de apenas um caractere
+
+        printf("Cor do Territorio:");
+        fgets(territorios[i].cor,TAM_COR,stdin);                    // solitica a informação da cor do territorio    
+        territorios[i].cor[strcspn(territorios[i].cor,"\n")] = '\0';  // a diferenca de uso da aspa dupla e simples deve-se ao fato de um lugar se referir a cadeia de strings e outro de apenas um caractere
+        
+        printf("Quantidade de Tropas no Territorio:");
+        scanf("%d",&territorios[i].tropas);                         // solitica a informação da qtd de tropas e usa scanf para recdeber um dado numerico   
+        
+        printf("------------------------------\n\n");               // so par efeito estético e separaçãod e blocos de entrada
+
+        getchar(); // para limpar o buffer do teclado
+      
+    }   // fim do for de entrada de dados
+
+    // COMENTARIO DO ALUNO: aqui eu apresento os dados entrados depois de cadastrados todos os territorios
+
+
+    printf("=============================================\n");
+    printf("           TERRITORIOS CADASTRADOS\n");
+    printf("=============================================\n\n");
+
+    for (i = 0; i < QTD_TERRITORIOS; i++){
+        printf("Territorio: %d\n",i+1);
+        printf("Nome do Territorio: %s\n",territorios[i].nome);                   // imprime a string com o nome do territorio
+        printf("Cor do Territorio: %s\n", territorios[i].cor);                    // imprime a string com a cor do territorio    
+        printf("Quantidade de Tropas no Territorio: %d\n",territorios[i].tropas); // imprime a informação da qtd de tropas como formato de inteiro   
+        printf("------------------------------\n\n");               // so par efeito estético e separaçãod e blocos de entrada
+      
+    }   // fim do for de apresentação do dados cadastrados
+
+    printf("\n****  Cadastro concluido com sucesso!  ***\n");
 
     return 0;
 }
